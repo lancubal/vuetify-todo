305c1d417f7d9d0bb374824be58c8d874531e1fc
Luna Lancuba <lunalancuba.sistemas@gmail.com>
2021-04-14 14:27:31 -0300

    Initial commit

---
+++ src/components/Shared/Snackbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-snackbar
+      v-model="$store.state.snackbar.show"
+    >
+      {{ $store.state.snackbar.text }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="primary"
+          text
+          v-bind="attrs"
+          @click="$store.commit('hideSnackbar')"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+</template>
+
+
+<style lang="sass">
+  div.v-snack:not(.v-snack--absolute)
+    height: 100%
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about pa-16">
+    <h1>About {{$store.state.appTitle}}</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil nostrum officia, voluptatem velit, expedita quis dicta ad hic similique dolores beatae, molestias commodi. Ut eius hic ex, quasi animi placeat.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil nostrum officia, voluptatem velit, expedita quis dicta ad hic similique dolores beatae, molestias commodi. Ut eius hic ex, quasi animi placeat.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil nostrum officia, voluptatem velit, expedita quis dicta ad hic similique dolores beatae, molestias commodi. Ut eius hic ex, quasi animi placeat.</p>
+  </div>
+</template>
